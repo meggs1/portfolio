e5792078c932aa1f8c407c953c9bb0c9880cbd3e
megan <megan.schertz@gmail.com>
2022-04-06 16:24:41 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import { About } from './components/about'
+import { Contact } from './components/contact'
+import { Projects } from './components/projects';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 
 function App() {
@@ -6,6 +10,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         Megan Schertz
+        <BrowserRouter>
+          <Link to="about">About Me</Link>
+          <Link to="projects">My Projects</Link>
+          <Link to="contact">Contact Me</Link>
+
+          <Routes>
+            <Route path="about" element={<About />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="contact" element={<Contact />} />
+          </Routes>        
+        </BrowserRouter>
         <img src="/buff-arya.jpg" alt="cat" width="400" height="600"/>
       </header>
     </div>

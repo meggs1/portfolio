70b3412ee01b1a23f52f20020067d3be0fa61313
megan <megan.schertz@gmail.com>
2022-04-07 15:53:09 -0500

    add home component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,24 @@ import './App.css';
 import { About } from './components/about'
 import { Contact } from './components/contact'
 import { Projects } from './components/projects'
+import { Home } from './components/home'
 import { NavBar } from './components/navbar'
 import { Routes, Route } from 'react-router-dom';
-// import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Megan Schertz
-
+        <div>
           <NavBar />
           <Routes>
+            <Route exact path="/" element={<Home />} />
             <Route exact path="about" element={<About />} />
             <Route exact path="projects" element={<Projects />} />
             <Route exact path="contact" element={<Contact />} />
           </Routes>  
-
-        <>
-
-        </>     
+        </div>
+        Megan Schertz   
         <img src="/msse.png" alt="logo"/>
         {/* <img src="/mscircle.png" alt="logo"/> */}
       </header>

5f36036a614b9e0637fe2d38581fa15ca4b8c937
megan <megan.schertz@gmail.com>
2022-04-07 16:47:27 -0500

    add bootstrap and update navbar to a dropdown list

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,6 @@
 export const Home = () => {
     return (
         <div>
-            Megan Schertz
-            <br></br>   
             <img src="/msse.png" alt="logo"/>
         </div>
     )
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,11 +2,47 @@ import {  Link } from "react-router-dom";
 
 export const NavBar = () => {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="about">About</Link>
-            <Link to="projects">Projects</Link>
-            <Link to="contact">Contact</Link>
+        // <nav class="navbar navbar-expand-lg navbar-light">
+        //       <div class="container-fluid">
+        //         <a class="navbar-brand" href="/">
+        //         <img src="/initals.png" alt="initials" width="30" height="24" class="d-inline-block align-text-top" />
+        //             Home
+        //         </a>
+        //     </div>
+        //     <div class="container-fluid">
+        //         <Link to="about">About</Link>
+        //         <Link to="projects">Projects</Link>
+        //         <Link to="contact">Contact</Link>
+        //     </div>
+        // </nav>
+
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <div>
+                <a class="navbar-brand" href="/">
+                    <img src="/initals.png" alt="initials" width="30" height="24" class="d-inline-block align-text-top" />
+                </a>
+            </div>
+            <div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavLightDropdown" aria-controls="navbarNavLightDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="navbarNavLightDropdown">
+                <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Menu
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">{<Link to="about">About</Link>}</a></li>
+                    <li><a class="dropdown-item" href="#">{<Link to="projects">Projects</Link>}</a></li>
+                    <li><a class="dropdown-item" href="#">{<Link to="contact">Contact</Link>}</a></li>
+                    </ul>
+                </li>
+                </ul>
+            </div>
+            </div>
         </div>
+    </nav>
     )
 } 

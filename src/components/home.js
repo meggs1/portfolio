3aa4100794f93d46549b85dffd3cb2fbe2d3e148
megan <megan.schertz@gmail.com>
2022-04-08 17:17:32 -0500

    fix DOM errors

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 export const Home = () => {
     return (
-        <div class="container-fluid img-max">
-            <img src="/msse.png" alt="logo" class="img-fluid"/>
+        <div className="container-fluid img-max">
+            <img src="/msse.png" alt="logo" className="img-fluid"/>
         </div>
     )
 } 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,35 +6,35 @@ import { Projects } from './projects'
 export const NavBar = () => {
     return (
         <div>
-        <nav class="navbar navbar-expand navbar-light d-flex fixed-top">
-            <div class="container-fluid d-flex">
+        <nav className="navbar navbar-expand navbar-light d-flex fixed-top">
+            <div className="container-fluid d-flex">
                 <div>        
-                    <a class="navbar-brand" href="/">
-                        <img src="/initals.png" alt="initials" width="30" height="24" class="d-inline-block align-text-top" />
+                    <a className="navbar-brand" href="/">
+                        <img src="/initals.png" alt="initials" width="30" height="24" className="d-inline-block align-text-top" />
                     </a>
                 </div>
                 <div>
-                    <div class="collapse navbar-collapse" id="navbarNavLightDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item dropdown">    
-                                <a class="nav-link " href="/#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div className="collapse navbar-collapse" id="navbarNavLightDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item dropdown">    
+                                <a className="nav-link " href="/#" id="navbarLightDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Menu &nbsp;
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                                        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                                     </svg>
                                 </a>
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarLightDropdownMenuLink">
+                                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarLightDropdownMenuLink">
                                     <li>
-                                        <a class="nav-link" href="#about">About</a>
+                                        <a className="nav-link" href="#about">About</a>
                                     </li>
                                     <li>
-                                        <a class="nav-link" href="#projects">Projects</a>
+                                        <a className="nav-link" href="#projects">Projects</a>
                                     </li>
                                     <li>
-                                        <a class="nav-link" href="#contact">Contact</a>
+                                        <a className="nav-link" href="#contact">Contact</a>
                                     </li>
                                     <li>
-                                        <a class="nav-link" href="#top">Back to Top</a>
+                                        <a className="nav-link" href="#top">Back to Top</a>
                                     </li>
                                 </ul>
                             </li>
@@ -44,14 +44,14 @@ export const NavBar = () => {
             </div>
         </nav>
 
-        <div data-bs-spy="scroll" data-bs-target="#navbar" data-bs-offset="0" class="scrollspy" tabindex="0">
-            <p id="top"><Home /></p>
+        <div data-bs-spy="scroll" data-bs-target="#navbar" data-bs-offset="0" className="scrollspy" tabIndex="0">
+            <div id="top"><Home /></div>
             <h4 id="about">About Me</h4>
-                <p><About /></p>
+                <div><About /></div>
             <h4 id="projects">Projects</h4>
-                <p><Projects /></p>
+                <div><Projects /></div>
             <h4 id="contact">Contact</h4>
-                <p><Contact /></p>
+                <div><Contact /></div>
         </div>
     </div>
     )
